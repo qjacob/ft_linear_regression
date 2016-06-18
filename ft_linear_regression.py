3b6a9380d46059864f7b3cff78adccdf77358957
qjacob <qjacob@student.42.fr>
2016-06-18 17:40:50 +0200

    Update ft_linear_regression.py

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -4,12 +4,12 @@
 #	for exemple in this exercice we have this Data set:
 #                --------------------
 #                |   km   |  Price  |
-#                 |--------|---------|
+#               |--------|---------|
 #         i = 1	| 240000 |  3650   |
 #         i = 2	| 139800 |  3800   |
 #         i = 3	| 150500 |  4400   |
 #         i = 4	| 185530 |  4450   |
-#         i = ...	|  ...	 |  ...    |
+#      i = ...	|  ...	 |  ...    |
 #                 --------------------
 #
 #																	'x' is the price. 'y' is the km. And 'i' is the index where we are in our data set.
