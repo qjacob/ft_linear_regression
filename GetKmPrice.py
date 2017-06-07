38ea62e25c9fbfb25424faed815e2acf9076c48e
quentin.jacob <qjacob@student.42.fr>
2017-06-07 18:40:06 +0200

    correct parsing data

--- GetKmPrice.py
+++ GetKmPrice.py
@@ -2,14 +2,34 @@
 
 import string
 import re
-mystr = open("data.csv")
 
-km = []
-price = []
+def getKm():
+	km = []
 
-for line in open("data.csv").readlines():
-	index = line.index(',');
-	tmp0 = line[:index]
-	tmp1 = line[index + 1:-1]
-	km.append(tmp0)
-	price.append(tmp1)
+	for line in open("data.csv").readlines():
+		index = line.index(',')
+		tmp = line[:index]
+		km.append(tmp)
+	km = km[1:]
+	return (km)
+
+def getPrice():
+	price = []
+
+	for line in open("data.csv").readlines():
+		index = line.index(',')
+		tmp = line[index + 1: -1]
+		price.append(tmp)
+	price = price[1:]
+	return (price)
+
+def get_len_dataset():
+	km = []
+	price = []
+	km = getKm()
+	price = getPrice()
+	if (len(km) != len(price)):
+		print "Error: Your data set is incorrect"
+		sys.exit(0)
+	m = len(km)
+	return(int(m))
