d2c86e0bde0e759a3d7ca5f93d9f5e711d8b7b17
quentin.jacob <qjacob@student.42.fr>
2017-06-07 18:40:30 +0200

    gradient descent used

---
+++ GradientDescent.py
@@ -0,0 +1,26 @@
+# repeat until convergence: {
+# 	Theta0 = Theta0 - LearningRate * (1 / m) * Sum(Hypothesis(x_i) - y_i)
+# 	Theta1 = Theta1 - LearningRate * (1 / m) * Sum(Hypothesis(x_i) - y_i)
+# }
+
+from compute import SumTheta0, SumTheta1
+import math
+import sys
+
+def GradientDescent(theta0, theta1, mileage, LearningRate, km, price, m):
+	theta0 = 0.0
+	theta1 = 0.0
+	while (True):
+		if math.isnan(theta1) or math.isnan(theta0):
+			print("OGINB !")
+			sys.exit(1)
+		tmp_theta0 = LearningRate * (1 / m * SumTheta0(theta0, theta1, km, price, m))
+		tmp_theta1 = LearningRate * (1 / m * SumTheta1(theta0, theta1, km, price, m))
+		# print(tmp_theta0)
+		# print(tmp_theta1)
+		if math.isnan(tmp_theta1) or math.isnan(tmp_theta0):
+			print("BINGO !")
+		if abs(tmp_theta0) < float(0.001) and abs(tmp_theta1) < float(0.001):
+			return (tmp_theta0, tmp_theta1)
+		theta0 = theta0 - tmp_theta0
+		theta1 = theta1 - tmp_theta1
