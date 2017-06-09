75160ca950d4b3979ff3d7fea89d19a91172ea19
quentin.jacob <qjacob@student.42.fr>
2017-06-09 18:45:02 +0200

    print some message and display value of Theta + result value

--- main.py
+++ main.py
@@ -5,27 +5,30 @@ from GradientDescent import GradientDescent
 
 def main():
 	LearningRate = 0.0001
+	LearningRate2 = 0.0001
 	theta0 = 0.
 	theta1 = 0.
 	price = []
 	km = []
 	mileage = raw_input("Please enter a mileage: ")
-	# try:
-	mileage = mileage[:-1]
-	float(mileage)
-	m = get_len_dataset()
-	price = getPrice()
-	km = getKm()
-	(hypothesis(theta0, theta1, m))
-	theta0, theta1 = (GradientDescent(theta0, theta1, mileage, LearningRate, km, price, m))
-	print("theta0:")
-	print(theta0)
-	print("theta1:")
-	print(theta1)
-	# except ValueError as e:
-	# 	print(e)
-	# 	print("error: Please enter an integer.")
-	# 	main()
+	try:
+		mileage = mileage[:-1]
+		float(mileage)
+		m = get_len_dataset()
+		price = getPrice()
+		km = getKm()
+		(hypothesis(float(theta0), float(theta1), float(m)))
+		theta0, theta1 = (GradientDescent(float(theta0), float(theta1), float(LearningRate), float(LearningRate2), km, price, m))
+		print("theta0:")
+		print(theta0)
+		print("theta1:")
+		print(theta1)
+		print("Result of hypothesis:")
+		print(hypothesis(theta0, theta1, float(mileage)))
+	except ValueError as e:
+		print(e)
+		print("error: Please enter an integer.")
+		main()
 
 main()
 
