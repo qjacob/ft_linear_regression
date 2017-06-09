e15b75c920b8b5ed3803a36dcbecac4ca11fb534
quentin.jacob <qjacob@student.42.fr>
2017-06-09 18:45:47 +0200

    nothing modify since last push

--- tools.py
+++ tools.py
@@ -5,13 +5,13 @@ def hypothesis(theta0, theta1, x):
 	return (theta0 + (theta1 * x))
 
 def SumTheta0(theta0, theta1, km, price, m):
-	value = 0.0
-	for i in range(m):
-		value += hypothesis(theta0, theta1, float(km[i])) - float(price[i])
+	value = 0.
+	for i in range(0, m):
+		value += hypothesis(theta0, theta1, km[i]) - price[i]
 	return (value)
 
 def SumTheta1(theta0, theta1, km, price, m):
-	value = 0.0
-	for i in range(m):
-		value += (hypothesis(theta0, theta1, float(km[i])) - float(price[i])) * float(km[i])
+	value = 0.
+	for i in range(0, m):
+		value += (hypothesis(theta0, theta1, km[i]) - price[i]) * km[i]
 	return (value)
