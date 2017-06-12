ecda686b025f31bfb403439c704c35441982ce31
quentin.jacob <qjacob@student.42.fr>
2017-06-12 16:13:26 +0200

    create file with our theta after trainning

--- GradientDescent.py
+++ GradientDescent.py
@@ -4,6 +4,7 @@
 # }
 
 from tools import SumTheta0, SumTheta1
+from GetKmPrice import get_len_dataset, getPrice, getKm
 import math
 import sys
 
@@ -22,3 +23,13 @@ def GradientDescent(theta0, theta1, LearningRate, LearningRate2, km, price, m):
 
 		theta0 = theta0 - tmp_theta0
 		theta1 = theta1 - tmp_theta1
+
+theta0, theta1 = GradientDescent(theta0=0.0, theta1=0.0, LearningRate=0.0001, LearningRate2=0.0001, km=getKm(), price=getPrice(), m=get_len_dataset())
+
+if (theta0 != 0.0) and (theta1 != 0.0):
+			try:
+				theta_value_file = open('theta_value_file', 'w')
+				theta_value_file.write('theta0={}\ntheta1={}'.format(theta0, theta1))
+				theta_value_file.close()
+			except Exception:
+				print("message: An error happened !")
