67a7a6174f251c547b76beaab4779b299785bcfd
quentin.jacob <qjacob@student.42.fr>
2017-08-01 07:48:14 +0200

    add graph display and option to ask for it

--- GradientDescent.py
+++ GradientDescent.py
@@ -7,14 +7,34 @@ from tools import SumTheta0, SumTheta1
 from GetKmPrice import get_len_dataset, getPrice, getKm
 from tools import hypothesis
 import matplotlib.pyplot as plt
-# import numba as nb
 import math
-import time
+import sys
 
-# @nb.jit(nb.types.Tuple((nb.float64[:], nb.float64[:,:]))(nb.float64[:], nb.float64[:,:])(nb.float64[:], nb.float64[:,:])(nb.float64[:], nb.float64[:,:]),nopython=True)
-def GradientDescent(theta0, theta1, LearningRate, LearningRate2, km, price, m):
+plt.ion()
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+def displayGraph(t0, t1, km, price, clear=False):
+
+	km = [x * 1000. for x in km]
+	price = [x * 1000. for x in price]
+	
+	if clear:
+		ax.clear()
+
+	ax.plot(km, price, 'o', color='#008080')
+
+	lineX = [0, 260000]
+	lineY = [hypothesis(t0, t1, 0), hypothesis(t0, t1, 260000)]
+
+	ax.plot(lineX, lineY, linestyle='dashed', color='#ff6600', linewidth=3.0)
+	plt.pause(0.001)
+
+def GradientDescent(theta0, theta1, LearningRate, LearningRate2, km, price, m, graph):
 	theta0 = 0.0
 	theta1 = 0.0
+
+	i = 0
 
 	while (True):
 		sum0 = SumTheta0(theta0, theta1, km, price, m) / float(m)
@@ -25,36 +45,28 @@ def GradientDescent(theta0, theta1, LearningRate, LearningRate2, km, price, m):
 		if abs(tmp_theta0) < float(0.000001) and abs(tmp_theta1) < float(0.000001):
 			return (theta0 * 1000, theta1)
 
+		if graph:
+			if i == 2000:
+				i = 0
+				displayGraph(theta0 * 1000, theta1, km, price, True)
+			else:
+				i += 1
+
 		theta0 = theta0 - tmp_theta0
 		theta1 = theta1 - tmp_theta1
+
+
+
+graph = 'graph' in sys.argv
 
 km = getKm()
 price = getPrice()
 
-start_time = time.time()
+theta0, theta1 = GradientDescent(theta0=0.0, theta1=0.0, LearningRate=0.0001, LearningRate2=0.0001, km=km, price=price, m=get_len_dataset(), graph=graph)
 
-theta0, theta1 = GradientDescent(theta0=0.0, theta1=0.0, LearningRate=0.0001, LearningRate2=0.0001, km=km, price=price, m=get_len_dataset())
-
-print(type(GradientDescent))
-
-end_time = time.time()
-
-print("Time to compute the GradientDescent: %s sec" % (end_time - start_time))
-
-km = [x * 1000. for x in km]
-price = [x * 1000. for x in price]
-
-lineX = [0, 260000]
-lineY = [hypothesis(theta0, theta1, 0), hypothesis(theta0, theta1, 260000)]
-
-start_graph = time.time()
-
-plt.plot(km, price, 'ro')
-plt.plot(lineX, lineY, 'r-')
-end_graph = time.time()
-
-print("Time to create graph: %s" % (end_graph - start_graph))
-plt.show()
+if graph:
+	plt.ioff()
+	plt.show()
 
 
 if (theta0 != 0.0) and (theta1 != 0.0):
